6905a1068a4d0cea16e0902348dee0fcfb365264
sohelmalek <sohelmalek03@gmail.com>
2022-05-22 21:35:29 +0530

    Updates

--- sohel.js
+++ sohel.js
@@ -14,14 +14,14 @@ $(function () {
   $(document).ready(function () {
     $(".navbar-toggler").click(function () {
       $(".offcanvas-collapse").toggleClass("open");
-      // $("body").toggleClass("offscroll");
+    $("body").toggleClass("offscroll");
     });
       $(".hamburger").click(function(){
         $(this).toggleClass("is-active");
       });
       $(".navbar-nav li a").click(function(){
         $(".navbar-collapse").removeClass("open");
-      // $("body").toggleClass("offscroll");
+        // $("body").toggleClass("offscroll");
   
       });
   
