3b44c6a2fd7a9edd072cbec1583f67688dfabab2
sohelmalek <sohelmalek03@gmail.com>
2022-05-22 19:43:08 +0530

    Updates

---
+++ sohel.js
@@ -0,0 +1,29 @@
+$(function () {
+    var header = $(".navbar");
+  
+    $(window).scroll(function () {
+      if ($(this).scrollTop() > 10) {
+        $(header).addClass('header_scroll');
+      } else {
+        $(header).removeClass('header_scroll');
+      }
+    });
+  
+  });
+  
+  $(document).ready(function () {
+    $(".navbar-toggler").click(function () {
+      $(".offcanvas-collapse").toggleClass("open");
+      $("body").toggleClass("offscroll");
+    });
+      $(".hamburger").click(function(){
+        $(this).toggleClass("is-active");
+      });
+      $(".navbar-nav li a").click(function(){
+        $(".navbar-collapse").removeClass("open");
+      // $("body").toggleClass("offscroll");
+  
+      });
+  
+    
+  });
